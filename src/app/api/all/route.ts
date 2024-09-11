a56418f720f8ee601ca2eac8a51d1cf32ebae9ab
Yash Bishnoi <112739888+nikhilDelu@users.noreply.github.com>
2024-09-11 10:12:21 +0530

    removed unused file

--- src/app/api/all/route.ts
+++
@@ -1,9 +0,0 @@
-import { prismaClient } from "@/lib/db";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest) {
-  const streams = await prismaClient.stream.findMany();
-  return NextResponse.json({
-    streams,
-  });
-}
